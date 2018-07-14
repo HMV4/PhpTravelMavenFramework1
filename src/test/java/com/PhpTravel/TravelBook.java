9147a3f356d7df5e0b34b214639d52df500a9107
HMV <hituj.velukar4@gmail.com>
2018-07-14 23:56:53 +0530

    Changes oush
    Changed commite

--- src/test/java/com/PhpTravel/TravelBook.java
+++ src/test/java/com/PhpTravel/TravelBook.java
@@ -23,7 +23,7 @@ import SnapShotUtility.SnapshotUtilty;
 
     public class TravelBook {
         static WebDriver driver;;;;;;;;;;;
-        static WebDriverWait wait_10;;;;;;;
+        static WebDriverWait wait_10;
         static Properties prop=null;
 
         @BeforeSuite
@@ -40,7 +40,7 @@ import SnapShotUtility.SnapshotUtilty;
         	//FileInputStream configFile=new FileInputStream("D:\\workspace\\PhpTravelMavenFramework1\\TravelConfig.properties");
         	prop.load(configFile);
         	
-            driver = new ChromeDriver();;;//
+            driver = new ChromeDriver();;;//Driver
            
             //driver.manage().window().maximize();
             // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
